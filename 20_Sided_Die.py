c11f8e5e7779eff3f1a392b238cf77530eb43df7
ThePoggerFrogger <93881554+MachPython@users.noreply.github.com>
2022-03-29 11:32:46 -0700

    Update 20_Sided_Die.py

--- 20_Sided_Die.py
+++ 20_Sided_Die.py
@@ -1,3 +1,21 @@
 import random
 print("your roll is>> ")
 print(random.randrange(1,20))
+
+import random
+def randNum(n):
+    repeat = []
+    print("your roll is>> ")
+    print(random.randrange(1,20))
+    reroll = input("would you like to roll again y/n--")
+    if reroll = 'y':
+        return repeat
+    elif reroll = 'n':
+        quit('goodbye')
+
+        #just for a single roll
+#import random
+#print("your roll is>> ")
+#print(random.randrange(1,20))
+
+        
